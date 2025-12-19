0ff87730750ace0ff2bb2adcf0e4533989b74bc7
abteilung6 <moeller.tilman@gmail.com>
2025-12-19 15:35:50 +0000

    load default config for aws sdk

--- cmd/aws-cli/main.go
+++ cmd/aws-cli/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/config"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	ctx := context.Background()
+
+	cfg, err := config.LoadDefaultConfig(ctx, config.WithRegion("eu-west-1"))
+	if err != nil {
+		log.Fatalf("Failed to load AWS config: %v", err)
+	}
+
+	fmt.Printf("AWS Config loaded successfully!\n")
+	fmt.Printf("Region: %s\n", cfg.Region)
 }
